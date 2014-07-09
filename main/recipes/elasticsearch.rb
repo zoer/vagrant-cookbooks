3d46740981d278ae02db5abfccb2d26df5c3fa7d
Yashchuk Oleg <oazoer@gmail.com>
2014-07-09 08:35:42 +0400

    Update elasticsearch to 1.2.1

--- main/recipes/elasticsearch.rb
+++ main/recipes/elasticsearch.rb
@@ -3,7 +3,7 @@ include_recipe "elasticsearch"
 elasticsearch_dir = "#{node[:elasticsearch][:dir]}/elasticsearch"
 bash "morphology plugin" do
   cwd elasticsearch_dir
-  code "bin/plugin -install analysis-morphology -url http://dl.bintray.com/content/imotov/elasticsearch-plugins/elasticsearch-analysis-morphology-1.1.0.zip"
+  code "bin/plugin -install analysis-morphology -url http://dl.bintray.com/content/imotov/elasticsearch-plugins/org/elasticsearch/elasticsearch-analysis-morphology/1.2.0/elasticsearch-analysis-morphology-1.2.0.zip"
   not_if { File.exists?("#{elasticsearch_dir}/plugins/analysis-morphology") }
   notifies :restart, "service[elasticsearch]"
 end
